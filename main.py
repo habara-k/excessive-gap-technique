fefa00d3f63fdbb6e32c16c13abe2780b69685c4
Keigo Habara <k.habara.aa@gmail.com>
2022-06-14 03:08:37 +0900

    Implement: No-Regret Learning (Multiplicative Weights Update)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import datetime
 
 from utils import *
-from no_regret import no_regret_learning
+from no_regret import online_gradient_descent, multiplicative_weights_update
 from grad_map import gradient_mapping
 from breg_proj import bregman_projection
 
@@ -11,10 +12,13 @@ from breg_proj import bregman_projection
 parser = argparse.ArgumentParser()
 parser.add_argument('--step', '-s', type=int, default=10000)
 parser.add_argument('--output', '-o', default='result.png')
+parser.add_argument('--seed', type=int)
 args = parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
+    if args.seed is not None:
+        np.random.seed(args.seed)
 
     plt.xscale('log')
     plt.yscale('log')
@@ -25,17 +29,24 @@ if __name__ == '__main__':
     split = 1000
     mask = [int(split * np.log(t+1) / np.log(args.step)) > int(split * np.log(t) / np.log(args.step)) for t in step]
 
-
     A = np.random.rand(4, 5)
-    nash_conv = np.array(no_regret_learning(A, step=args.step))
-    plt.plot(step[mask], nash_conv[mask], label='no regret')
+    nash_conv = np.array(online_gradient_descent(A, step=args.step))
+    plt.plot(step[mask], nash_conv[mask], label='no-regret(OGD)')
+
+    nash_conv = np.array(multiplicative_weights_update(A, step=args.step))
+    plt.plot(step[mask], nash_conv[mask], label='no-regret(MWU)')
 
     nash_conv = np.array(gradient_mapping(A, step=args.step))
-    plt.plot(step[mask], nash_conv[mask], label='grad map(euclid)')
+    plt.plot(step[mask], nash_conv[mask], label='gradient mapping(euclid distance)')
 
     nash_conv = np.array(bregman_projection(A, step=args.step))
-    plt.plot(step[mask], nash_conv[mask], label='breg proj(entropy)')
+    plt.plot(step[mask], nash_conv[mask], label='bregman projection(entropy disance)')
 
     plt.legend()
     plt.grid()
-    plt.savefig(args.output)
+    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    plt.savefig('fig/' + now + '-' + args.output)
+
+
+if __name__ == '__main__':
+    main()
--- no_regret.py
+++ no_regret.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from utils import *
 
 
-def no_regret_learning(A, step):
+def online_gradient_descent(A, step):
     (n,m) = A.shape
     x = np.full(n, 1/n)
     u = np.full(m, 1/m)
@@ -17,6 +17,44 @@ def no_regret_learning(A, step):
         eta = 1 / np.sqrt(t)
         x_nxt = euclidean_projection_onto_simplex(x - eta * A @ u)
         u_nxt = euclidean_projection_onto_simplex(u + eta * x @ A)
+
+        x = x_nxt
+        u = u_nxt
+
+        x_sum += x
+        u_sum += u
+
+        x_avg = x_sum / (t+1)
+        u_avg = u_sum / (t+1)
+
+        assert LA.norm(x_avg - euclidean_projection_onto_simplex(x_avg)) < 1e-9
+        assert LA.norm(u_avg - euclidean_projection_onto_simplex(u_avg)) < 1e-9
+        x_avg = euclidean_projection_onto_simplex(x_avg)
+        u_avg = euclidean_projection_onto_simplex(u_avg)
+
+        nash_conv.append(np.max(x_avg @ A) - np.min(A @ u_avg))
+
+    return nash_conv
+
+
+def multiplicative_weights_update(A, step):
+    (n,m) = A.shape
+    x = np.full(n, 1/n)
+    u = np.full(m, 1/m)
+    x_sum = x
+    u_sum = u
+
+    nash_conv = [np.max(x_sum @ A) - np.min(A @ u_sum)]
+
+    for t in tqdm(range(1, step)):
+        beta_1 = 1 / (1 + np.sqrt(2 * np.log(n) / int(np.sqrt(t))**2))
+        beta_2 = 1 / (1 + np.sqrt(2 * np.log(m) / int(np.sqrt(t))**2))
+
+        x_nxt = x * np.power(beta_1, A @ u)
+        x_nxt /= np.sum(x_nxt)
+
+        u_nxt = u * np.power(beta_2, -x @ A)
+        u_nxt /= np.sum(u_nxt)
 
         x = x_nxt
         u = u_nxt
